74845c8668875a824a9fda3d8821dedade9eff4f
Jabir <jabirasd@gmail.com>
2020-08-20 14:24:44 +0530

    Updates

---
+++ precache-manifest.6c394677a88d85fd3581e41c5f72f05d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64fc25fc72b8574364adfa6ff271fb8",
+    "url": "/Jabi-Jaleel/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4ffccb9711e27e0657b8",
+    "url": "/Jabi-Jaleel/tic-tac-toe/static/css/main.7e65bb18.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/Jabi-Jaleel/tic-tac-toe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jabi-Jaleel/tic-tac-toe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffccb9711e27e0657b8",
+    "url": "/Jabi-Jaleel/tic-tac-toe/static/js/main.6cf7f740.chunk.js"
+  },
+  {
+    "revision": "a84cbe164a58b44313ed",
+    "url": "/Jabi-Jaleel/tic-tac-toe/static/js/runtime-main.9bd07cd8.js"
+  }
+]);
